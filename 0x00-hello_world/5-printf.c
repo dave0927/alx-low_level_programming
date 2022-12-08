695d51d0a12c024898c30382350e9a1968e7ee49
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-08 10:03:43 -0800

    5-printf.c file created

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+/**
+* main - prints some text
+* Description: C programm that prints a text exactly as is
+* Return: 0
+*/
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
