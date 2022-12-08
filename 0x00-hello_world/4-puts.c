2c3efc1370e8c5ed3b70d14e173de2e51117d6cc
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-08 09:58:21 -0800

    done fixing

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,7 +1,8 @@
 #include <stdio.h>
 /**
-*entry point -> main()
-*always return 0
+* main - prints some strings
+* Description: A C program that prints with put function
+* Return: Always 0 (Success)
 */
 int main(void)
 {
