c82723c6f352f4608bf82ff43d00e0040e2c8335
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-13 10:49:05 -0800

    file added

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "main.h"
+
+/**
+* print_alphabet - prints the alphabet from a - z
+*/
+
+void print_alphabet(void)
+{
+	int _char = 'a';
+
+	for (_char; _char <= 'z'; _char++)
+		_putchar(_char);
+	_putchar('\n');
+}
