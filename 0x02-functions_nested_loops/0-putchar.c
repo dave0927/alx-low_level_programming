3b1afb1acf8be82683a62a4f7b5f34c45249d83d
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-13 10:32:19 -0800

    files added

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,12 +1,20 @@
 #include <stdio.h>
+
 /**
 * main - entry point
+*
 * Return: return 0
+*
 */
 
 int main(void)
 {
-    printf("_putchar\n");
+	char _string[] = "_putchar";
+	int ch = 0;
 
-    return 0;
+	for (ch; ch < 8; ch++)
+		_putchar(_string[ch]);
+	_putchar("\n");
+
+	return (0);
 }
