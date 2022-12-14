7136c5834bb532030adc04b576de9040cd139886
dave0927 <114774535+dave0927@users.noreply.github.com>
2022-12-14 17:50:52 +0300

    minor fix

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -9,13 +9,15 @@
 
 int main(void)
 {
-	char theString[] = "_putchar";
-
-	for (int _ch = 0; _ch < 8; _ch++)
-	{
-		_putchar(theString[_ch]);
-	}
-	_putchar("\n");
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
 
 	return (0);
 }
