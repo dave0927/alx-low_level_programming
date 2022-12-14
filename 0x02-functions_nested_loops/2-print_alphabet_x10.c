c975e8030ff2a8059c7ab532834b0ab35cfcc732
dave0927 <114774535+dave0927@users.noreply.github.com>
2022-12-14 19:56:30 +0300

    Update 2-print_alphabet_x10.c

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,16 +6,18 @@
 
 void print_alphabet_x10(void)
 {
-	int _printLine = 0, _char = 'a';
+	int _printLine = 0;
 
 	while (_printLine <= 9)
 	{
+		int _char = 'a';
+		
 		while (_char <= 'z')
 		{
 			_putchar(_char);
 			_char++;
 		}
 		_printLine++;
+		_putchar("\n");
 	}
-	_putchar("\n");
 }
