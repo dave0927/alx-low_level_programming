6e4a842c588fd54885934a8503a508d161e09a8c
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-15 09:56:03 -0800

    fix

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,7 +1,5 @@
 #include "main.h"
 
-void positive_or_negative(int i);
-
 /**
 * main - checks if an int is positive or not
 * Return: always 0
@@ -11,7 +9,7 @@ int main(void)
 {
 	int _number;
 
-	_number = 0;
+	_number = 2 % 2;
 	positive_or_negative(_number);
 
 	return (0);
