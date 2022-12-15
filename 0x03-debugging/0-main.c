191830f7987597fd1211e87c2e35f756eb93ebff
Dawit Tadesse <dawittadesse927@gmail.com>
2022-12-15 02:25:06 -0800

    file created

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#include "main.h"
+
+/**
+* main - checks if an int is positive or not
+* Return: always 0
+*/
+
+int main(void)
+{
+	int _number;
+
+	_number = 98 % 2;
+	positive_or_negative(_number);
+
+	return (0);
+}
