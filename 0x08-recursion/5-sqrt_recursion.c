28cfd8894a1ea086cda48ceed0983a340bd940c0
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-04 04:04:24 -0800

    file created

---
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * sqtRecursive - computes square root recursively doing binary search
+ * @start: inital number
+ * @end: last number within the limit of number
+ * @n: given number
+ * Return: 1 if not found sqrroot, else sqrroot
+ **/
+int sqtRecursive(int start, int end, int n)
+{
+	long mid;
+
+	if (end >= start)
+	{
+		mid = start + (end - start) / 2;
+		if (mid * mid == n)
+			return (mid);
+		if (mid * mid > n)
+			return (sqtRecursive(start, mid - 1, n));
+		if (mid * mid < n)
+			return (sqtRecursive(mid + 1, end, n));
+	}
+	return (-1);
+}
+	int _sqrt_recursion(int n)
+{
+	if (n < 0)
+		return (-1);
+	if (n == 0 || n == 1)
+		return (n);
+	return (sqtRecursive(2, n, n));
+}
