70d5a0fcd214107a01426c1f8e8cf2907d3ac6ef
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-16 05:54:16 -0800

    fix

--- 0x0D-preprocessor/3-function_like_macro.h
+++ 0x0D-preprocessor/3-function_like_macro.h
@@ -1,19 +1,4 @@
-#include "1-pi.h"
-#include "1-pi.h"
-#include <stdio.h>
-
-/**
- * main - check the code
- * Return: Always 0.
- */
-
-int main(void)
-{
-    float a;
-    float r;
-
-    r = 98;
-    a = PI * r * r;
-    printf("%.3f\n", a);
-    return (0);
-}
+#ifndef DOUBLEGUARD
+#define DOUBLEGUARD
+#define ABS(x) ((x) < 0 ? ((x) * (-1)) : (x))
+#endif
