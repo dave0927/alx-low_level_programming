315b6e02c665dbf387bfd7200c9ae55ef48113ee
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-19 09:17:54 -0800

    files added

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,9 +3,9 @@
 /**
  * sum_them_all - returns the sum of all its parameters.
  * @n: amount of the arguments.
+ *
  * Return: sum of its parameters.
  */
-
 int sum_them_all(const unsigned int n, ...)
 {
 	va_list valist;
