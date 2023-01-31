ed50f0665183171968b02ce007a3a0b5ecf9467d
Dawit Tadesse <dawittadesse927@gmail.com>
2023-01-31 01:39:40 -0800

    fix: format

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -14,7 +14,8 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 	listint_t *current, *next;
 
 	if (head == NULL || *head == NULL)
-	return (-1);
+		return (-1);
+
 	if (index == 0)
 	{
 		next = (*head)->next;
@@ -22,13 +23,16 @@ int delete_nodeint_at_index(listint_t **head, unsigned int index)
 		*head = next;
 		return (1);
 	}
+
 	current = *head;
+
 	for (i = 0; i < index - 1; i++)
 	{
 		if (current->next == NULL)
 			return (-1);
 			current = current->next;
 	}
+
 	next = current->next;
 	current->next = next->next;
 	free(next);
